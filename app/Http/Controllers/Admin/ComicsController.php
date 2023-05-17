a0259664605c17cdc932a3cb1391a0749bc60bcc
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-05-17 16:17:26 +0200

    comics/index creation

--- app/Http/Controllers/Admin/ComicsController.php
+++ app/Http/Controllers/Admin/ComicsController.php
@@ -15,7 +15,9 @@ class ComicsController extends Controller
      */
     public function index()
     {
-        //
+        $comics = Comic::all();
+
+        return view('comics.index', compact('comics'));
     }
 
     /**
---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+
+        {{-- Include Assets --}}
+        @vite('resources/js/app.js')
+    </head>
+
+    <body>
+        <div class="container">
+            @foreach ($comics as $comic)
+            <div class="card mt-5">
+                <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}">
+                <div class="card-body">
+                  <h5 class="card-title">{{ $comic->title }}</h5>
+                  <h6 class="card-subtitle mb-2 text-body-secondary">{{ $comic->type }} || {{ $comic->series }}</h6>
+                  <p class="card-text">{{ $comic->description }}</p>
+                </div>
+                <div class="card-footer text-body-secondary">
+                    {{ $comic->price }} || {{ $comic->sale_date }}
+                </div>
+            </div>
+            @endforeach
+        </div>
+        {{--
+            $table->string('artists')->nullable();
+            $table->string('writers')->nullable(); --}}
+    </body>
+</html>
+
+<style lang="scss" scoped>
+    .card {
+        width: 40%;
+    }
+</style>
