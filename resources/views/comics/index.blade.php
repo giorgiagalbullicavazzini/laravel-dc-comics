3df608fc097aa9a90c22dec8082218960127bb5e
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-05-17 21:13:27 +0200

    confirmation before delete

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -24,19 +24,19 @@
                             </div>
                             <div class="col-md-8">
                                 <div class="card-body">
-                                <h5 class="card-title">{{ $comic->title }}</h5>
-                                <h6 class="card-subtitle mb-2 text-body-secondary">{{ $comic->type }} || {{ $comic->series }}</h6>
-                                <p class="card-text">{{ \Illuminate\Support\Str::limit($comic->description, 70, $end = '...')}}</p>
-                                <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-primary">Details</a>
-                                <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-warning">Modify</a>
-                                <form action="{{ route('comics.destroy', $comic->id) }}" method="POST" class="d-inline">
-                                    @csrf
-                                    @method('DELETE')
+                                    <h5 class="card-title">{{ $comic->title }}</h5>
+                                    <h6 class="card-subtitle mb-2 text-body-secondary">{{ $comic->type }} || {{ $comic->series }}</h6>
+                                    <p class="card-text">{{ \Illuminate\Support\Str::limit($comic->description, 70, $end = '...')}}</p>
+                                    <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-primary">Details</a>
+                                    <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-warning">Modify</a>
+                                    <form action="{{ route('comics.destroy', $comic->id) }}" method="POST" class="d-inline">
+                                        @csrf
+                                        @method('DELETE')
 
-                                    <button class="btn btn-danger">Delete</button>
-                                </form>
+                                        <input type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you REALLY want to delete {{ $comic->title }} from the database?')" value="Delete">
+                                    </form>
+                                </div>
                             </div>
-                          </div>
                         </div>
                         <div class="card-footer text-body-secondary mt-auto">
                             ${{ $comic->price }} || <strong>Release Date:</strong> {{ $comic->sale_date }}
@@ -46,8 +46,5 @@
                 @endforeach
             </div>
         </div>
-        {{--
-            $table->string('artists')->nullable();
-            $table->string('writers')->nullable(); --}}
     </body>
 </html>
