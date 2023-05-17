15a31d2349435605f90cb49669f48abc6e0bdf21
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-05-17 15:46:08 +0200

    created model, migration and resource controller

---
+++ database/migrations/2023_05_17_131002_create_comics_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 150)->unique();
+            $table->text('description');
+            $table->string('thumb')->nullable();
+            $table->double('price', 4, 2)->unsigned();
+            $table->string('series', 50)->nullable();
+            $table->date('sale_date')->nullable();
+            $table->string('type', 30);
+            $table->string('artists')->nullable();
+            $table->string('writers')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
